7c479b22ed0aecf9a4d29946a72459fd5e9af950
Jerry W <jerry.wang.ct@gmail.com>
2016-04-19 23:29:06 -0400

    created function get_languages

--- testLanguageCode.py
+++ testLanguageCode.py
@@ -6,10 +6,16 @@ with open("sample_code.txt") as f:
     line = f.read()
     comments = line.split("\",\"")
 
-for comment in comments:
-    line = re.sub('\"','', comment)
-    try:
-        print guess_lexer(line)
-        #print line, "\n|\n|\n|\n"
-    except:        
-        print "no language found!"
+def get_languages(comments):
+
+    for comment in comments:
+        line = re.sub('\"','', comment)
+        try:
+            print guess_lexer(line)
+            #print line, "\n|\n|\n|\n"
+            return True
+        except:
+            pass
+    return False
+
+get_languages(comments)            
